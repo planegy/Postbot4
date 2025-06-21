7f58bef14bd8025fa69176bd9244955d5a72233d
planegy <planegy.de@gmail.com>
2025-06-21 14:23:45 +0200

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-from flask import Flask, ...
-from ... import load_dotenv
+rom flask import Flask, request, jsonify
+from dotenv import load_dotenv
+import os
 
-...
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    return "Planegy Postbot ist online."
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
